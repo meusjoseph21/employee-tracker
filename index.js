17ea5375e80fb53c705e55b98878aeb5dc9392a5
JosMuse <no-reply>
2020-10-28 16:38:33 -0600

    adding department selection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
+require("console.table")
 
 //sql connection
 const connection = mysql.createConnection({
@@ -43,16 +44,28 @@ function initQuestions(){
 function allEmployees(){
 
     console.log("Selecting all employees...\n");
-     connection.query("SELECT * FROM department", function(err, res) {
+     connection.query("SELECT employees.id, employees.first_name, employees.last_name, role.title, manager.first_name AS manager, role.salary, department.name AS Department FROM employees LEFT JOIN role ON employees.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employees manager ON manager.id = employees.manager_id", function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-    connection.end();
+    initQuestions();
   });
 
 }
 
 function departments(){
+    inquirer.prompt(
+        {
+            type: "list",
+            message: 'Which department would you like to view',
+            name: 'department',
+            choices: ["Human Resources", "Sales", "Customer Service" ]
+
+        }
+    ).then(function(response){
+        console.log(response)
+        console.log(`Selecting employees from ${response.department} department...`)
+    })
 
 }
 
