a5d4768e2b9cae7803b9fde1b89de37543f56038
JosMuse <no-reply>
2020-10-28 17:25:27 -0600

    added an emp[loyee add statemtn

--- index.js
+++ index.js
@@ -25,18 +25,23 @@ function initQuestions(){
             type: "list",
             message: "What would you like to do?",
             name: "start",
-            choices: ["view all employees", "view all employees by department", "view all employees by manager"]
+            choices: ["view all employees", "view all departments", "view all roles", "add employee", "add department", "exit"]
         }
     ).then(function(response){
         console.log(response)
         switch(response.start){
             case "view all employees" : allEmployees()
                 break
-            case "view all employees by department": departments()
+            case "view all departments": departments()
                 break
-            case "view all employees by manager": manager()
+            case "view all roles": roles()
                 break
-            default: postItem()
+            case "add employee": addEmployee()
+                break
+            case "add department": addDepartment()
+                break
+            case "exit": console.log("Goodbye")
+                break
         }
     })
 }
@@ -54,25 +59,69 @@ function allEmployees(){
 }
 
 function departments(){
-    inquirer.prompt(
-        {
-            type: "list",
-            message: 'Which department would you like to view',
-            name: 'department',
-            choices: ["Human Resources", "Sales", "Customer Service" ]
+   connection.query("SELECT department.name AS departments FROM department", function (err, res){
+       if (err) throw (err);
+       console.table(res)
 
-        }
-    ).then(function(response){
-        console.log(response)
-        console.log(`Selecting employees from ${response.department} department...`)
+        initQuestions()
+   })
+
+}
+
+function roles(){
+    connection.query("SELECT role.title AS Roles FROM role", function (err, res){
+        if (err) throw (err);
+        console.table(res)
+ 
+         initQuestions()
     })
 
 }
 
-function manager(){
+function addEmployee(){
+    inquirer.prompt([
+        
+            {
+                type: "input",
+                message: "Please enter a first name",
+                name: "first_name"
+              },
+              {
+                type: "input",
+                message: "Please enter a last name",
+                name: "last_name"
+              },
+              {
+                type: "number",
+                message: "Please select your role number",
+                name: "roleID"
+              },
+              {
+                type: "number",
+                message: "Please enter your managers ID",
+                name: "managerID"
+              }
+    
+    ]).then(function(response){
+        console.log("adding a new employee...\n");
+    connection.query("INSERT INTO employees SET ?",
+    {
+      first_name: response.first_name,
+      last_name: response.last_name,
+      role_id: response.roleID,
+      manager_id: response.managerID
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(`${response.first_name} ${response.last_name} added.`);
+      // Call updateProduct AFTER the INSERT completes
+      initQuestions();
+    }
+  );
 
+    })
+    
 }
-
 
 
 //initialize
